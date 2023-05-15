c85143bf887de6954a93f16db5f36513f0405a0b
Luciano Yomayel <luciano.yomayel@joinsolutions.net>
2023-05-15 16:46:02 -0300

    cambio en environment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://44.211.52.12:3050/'
-  apiUrl: 'http://localhost:3050/'
+  apiUrl: 'https://api-demo.gestion-horarios.com'
+  // apiUrl: 'http://localhost:3050/'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://44.211.52.12:3100/'
+  apiUrl: 'https://api-eesn14.gestion-horarios.com'
 };
